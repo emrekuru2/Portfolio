65a58a6e8f00217b184256b0bfcb36cf75188d32
Emre Kuru <kuru.emre@hotmail.com>
2023-08-31 11:14:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const base = (mode === "production") ? "/Portfolio/" : "./";
-
-  return {
-    plugins: [react()],
-    base: base
-  }
-
+export default defineConfig({
+  plugins: [react()]
 })
